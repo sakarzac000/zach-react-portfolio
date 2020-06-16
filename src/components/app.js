f5643fd095014f63f21178cf65cfbefbfeabd05a
Zach Sakar <zach.sakar@gmail.com>
2020-06-16 09:40:54 -0600

    Added Portfolio Item and Container

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import PortfolioContainer from './portfolio/portfolio-container';
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends Component {
         <div>
           { moment().format('MMMM Do YYYY, h:mm:ss a') }
         </div>
+        <PortfolioContainer />
       </div>
     );
   }
